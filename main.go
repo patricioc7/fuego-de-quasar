e124ea34589a48660252abadbde05e6073f23053
pcordoba <patricio@cliengo.com>
2020-10-04 15:38:28 -0300

    deleted unused files

--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"goCore/cache"
-	"goCore/config"
-	"goCore/db"
 	"goCore/server"
 )
 
 func main(){
-	config.InitFlagConfig()
-	db.InitConnection(&config.Fconfig)
 	cache.InitCache()
 	server.Init()
-
 }
